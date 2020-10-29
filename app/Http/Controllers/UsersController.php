1a0a5a4d5152d75477b454ad205f883b5e0c1161
CalDey <2234706312@qq.com>
2020-10-29 09:22:33 +0000

    邮箱验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -67,7 +67,7 @@ class UsersController extends Controller
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
         Mail::send($view, $data, function ($message) use ($to, $subject) {
-            $message->from($to)->subject($subject);
+            $message->to($to)->subject($subject);
         });
     }
 
